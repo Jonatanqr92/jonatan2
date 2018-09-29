42c6e1d15c5a3adaca451a81b37d6e9523a3dac5
jonatan <jonatanquinonesreyes@gmail.com>
2018-09-29 17:27:06 -0500

    first commit

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,34 @@
+class BookController < ApplicationController
+def index
+    #hacemos una consulta de todos los libros a la base de datos
+@books = Book.all
+end
+def new
+end
+
+
+def create
+    book_params = params.require(:book).permit(:title, :description)
+    Book.create(book_params)
+    redirect_to action: 'index'
+
+end
+def show
+    @book = Book.find(params[:id])
+     if @book.nil?
+        redirect_to action: 'index'
+
+     end
+end
+
+def delete
+    @book = Book.find(params[:id])
+    unless @book.nil?
+        @book.delete
+
+    end
+    redirect_to action: 'index'
+
+end
+
+end
